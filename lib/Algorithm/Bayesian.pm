4705a3272a4dc31958bf396f28551c5f215802a8
Gea-Suan Lin <darkkiller@gmail.com>
2010-11-24 18:49:37 +0800

    Remove BUGS and SUPPORT.

--- lib/Algorithm/Bayesian.pm
+++ lib/Algorithm/Bayesian.pm
@@ -189,42 +189,6 @@ sub testWord {
 
 Gea-Suan Lin, C<< <gslin at gslin.org> >>
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-algorithm-bayesian at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Algorithm-Bayesian>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Algorithm::Bayesian
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Algorithm-Bayesian>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Algorithm-Bayesian>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Algorithm-Bayesian>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Algorithm-Bayesian/>
-
-=back
-
-
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2010 Gea-Suan Lin.
